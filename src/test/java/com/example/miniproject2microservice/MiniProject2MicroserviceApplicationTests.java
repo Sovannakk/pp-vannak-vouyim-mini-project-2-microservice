dccfea9286b0b12086362cb6370ee4a11dd6915e
MacBook pro <macbookpro@Nak-2.local>
2023-09-27 10:07:41 +0700

    init

---
+++ src/test/java/com/example/miniproject2microservice/MiniProject2MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproject2microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProject2MicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
